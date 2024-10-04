0bf4b5695874bda12abeb956d07dd9273d800fc5
NGC.Y <applejapan0812@gmail.com>
2024-10-04 19:26:35 +0900

    use dataset

--- collect.py
+++ collect.py
@@ -1,4 +1,4 @@
-#收集自我对弈数据
+#收集数据
 import random
 import numpy as np
 from collections import deque
@@ -10,52 +10,57 @@ from network import PolicyValueNet
 import env_QCS
 from MCTS import MCTSplayer
 from config import CONFIG
-
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import Operator
 if CONFIG['use_redis']:
     import my_redis, redis
 
 # import zip_array
 
 class CollectPipeline:
-    def __init__(self,n_qubit, target, theta = CONFIG['theta'], init_model=None):
+    def __init__(self,n_qubit, theta = CONFIG['theta'], init_model=None):
         # 电路和合成环境
-        self.gate_Set = env_QCS.gate_Set
+        self.gate_Set = env_QCS.gate_Set # 直接用qiskit的
         self.n_qubit = n_qubit
-        self.target = target
         self.theta = theta
         # self.circuit = env_QCS.circuit(self.n_qubit, self.gate_Set, self.target, self.theta)
-        self.env = env_QCS.Sys2target(self.n_qubit, self.gate_Set, self.target)
+        # self.env = env_QCS.Sys2target(self.n_qubit, self.gate_Set, self.target)  #写的太麻烦了
         # 参数
-        self.temp = 1  # 温度
+        self.temp = CONFIG['temp']  # 温度
         self.n_playout = CONFIG['play_out']  # 每次移动的模拟次数
         self.c_puct = CONFIG['c_puct']  # u的权重
         self.buffer_size = CONFIG['buffer_size']  # 经验池大小
         self.data_buffer = deque(maxlen=self.buffer_size)
         self.iters = 0
+        self.inital_model = init_model
         if CONFIG['use_redis']:
             self.redis_cli = my_redis.get_redis_cli()
 
         # 从主体加载模型
-    def load_model(self, model_file=CONFIG["model_path"]):
+    def load_model(self):
             try:
-                self.policy_value_net = PolicyValueNet(model_file)
-                print('已加载最新模型')
+                if self.inital_model:
+                    self.policy_value_net = PolicyValueNet(self.inital_model)
+                    print('已加载最新模型')
+                else:
+                    self.policy_value_net = PolicyValueNet()
+                    print('已加载初始模型')
             except:
                 self.policy_value_net = PolicyValueNet()
                 print('已加载初始模型', self.policy_value_net)
-                # print(self.policy_value_net)
+            ## policy_value_fn
             self.mcts_player = MCTSplayer(self.policy_value_net.policy_value_fn,
                                           c_puct=self.c_puct,
                                           n_playout=self.n_playout,
                                           is_selfplay=1)
 
-    def collect_selfplay_data(self, n_games=1):
-        print("buffer size" ,self.buffer_size)
-
-        # 收集自我对弈的数据
+    def collect_selfplay_data(self, target, n_games=1):
+        # 收集当前模型，当前矩阵的数据
+        # n_games 处理几次当前矩阵
         for i in range(n_games):
             self.load_model()  # 从本体处加载最新模型
-            is_target, play_data = self.env.self_play(self.mcts_player, temp=self.temp)
+            env = env_QCS.Sys2target(self.n_qubit, self.gate_Set, target)
+            is_target, play_data = env.self_play(self.mcts_player, temp=self.temp)
             play_data = list(play_data)[:]
             self.episode_len = len(play_data)
 
@@ -85,27 +90,43 @@ class CollectPipeline:
         return self.iters
 
     def run(self):
-        """开始收集数据"""
+        """执行收集数据"""
+        with open(r"C:\projects\AlphaZero+QuantumCircuits\QCS_AlphaZero\dataset\intial_sequences.txt", "r") as file:
+            initial_circuits = [line.strip() for line in file.readlines()]
+
         try:
             while True:
-                iters = self.collect_selfplay_data()
-                print('batch i: {}, episode_len: {}'.format(
+                ## 随机产生目标矩阵
+                random_index = random.randint(0, len(initial_circuits) - 1)
+                initial_circuit = initial_circuits[random_index]
+                circuit = QuantumCircuit(1)
+                for gate in initial_circuit:
+                    if gate == "H":
+                        circuit.h(0)
+                    elif gate == "T":
+                        circuit.t(0)
+                    elif gate == "S":
+                        circuit.s(0)
+                    elif gate == "T_d":
+                        circuit.tdg(0)
+                    elif gate == "S_d":
+                        circuit.sdg(0)
+                circuit_unitary = Operator(circuit)
+                target = circuit_unitary.data
+
+                iters = self.collect_selfplay_data(target)
+                # iter i：第i次添加数据，episode_len：添加了几条数据
+                print('iter i: {}, episode_len: {}'.format(
                     iters, self.episode_len))
+
+
         except KeyboardInterrupt:
             print('\n\rquit')
 
 
 
 if __name__ == '__main__':
-    matrix = np.array([
-        [1, 0, 0, 0],
-        [0, 0, 1, 0],
-        [0, 1, 0, 0],
-        [0, 0, 0, 1]
-    ])
-
-
-    collecting_pipeline = CollectPipeline(2, matrix, init_model='current_policy.model')
+    collecting_pipeline = CollectPipeline(CONFIG['n_qubit'], CONFIG['theta'], CONFIG['model_path'])
     collecting_pipeline.run()
 
 
