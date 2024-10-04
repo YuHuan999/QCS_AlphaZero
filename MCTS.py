ec9c10701f26cadbe934838da9655cbd0f17ec9b
NGC.Y <applejapan0812@gmail.com>
2024-10-04 19:29:05 +0900

    correct MCTS

--- MCTS.py
+++ MCTS.py
@@ -21,11 +21,14 @@ class TreeNode(object):
         self.U = 0 # 当前节点的对应动作的置信上界 PUCT
 
     def expand(self, action_prob): # 非法动作概率为0
+        # print("创建新节点")
         #创建新的子节点 来展开树
+        # list = list(action_prob)
+        # print("all action", action_prob)
         for action, prob in action_prob:
             if action not in self.children:
                 self.children[action] = TreeNode(self, prob) # self：parent
-
+        # print("子节点", self.children)
 
     def get_value(self,c_puct):
         #c_puct：控制相对影响 大：倾向探索 小：倾向Q值
@@ -39,16 +42,14 @@ class TreeNode(object):
         return max(self.children.items(), key=lambda act_node: act_node[1].get_value(c_puct))
 
 
-    def update_recursive(self, leaf_value, rewards):
+    def update_recursive(self, leaf_value):
         if self.parent:
-            reward = rewards.pop() if rewards else 0  # 确保有奖励可用
-            # q(t) = r +  q(t+1)* alpha
-            Qs = CONFIG["alpha"] * (reward + leaf_value) + self.parent.Q * (self.parent.count_visit - 1)
+            Qs = CONFIG["alpha"] * leaf_value + self.parent.Q * (self.parent.count_visit)
             self.parent.count_visit += 1
             self.parent.Q = Qs / self.parent.count_visit
             # 更新当前的leaf_value为父节点的Q值
             leaf_value = self.parent.Q
-            self.parent.update_recursive(leaf_value, rewards)              #递归
+            self.parent.update_recursive(leaf_value)              #递归
 
 
     def is_leave(self):
@@ -60,46 +61,55 @@ class TreeNode(object):
 
 class MCTS(object):
 
-    def __init__(self, polic_value_fn, c_puct = 5, n_playout = 2000):
+    def __init__(self, policy_value_fn, c_puct = 5, n_playout = 2000):
     #polic_value_fn:神经网络提供:接受当前状态-》返回接下来的动作分布和当前状态的评分
         self.root = TreeNode(None, 1.0)
-        self.polic_value = polic_value_fn
+        self.policy_value = policy_value_fn
         self.c_puct = c_puct
         self.n_playout = n_playout
 
     def playout(self, circuit):
         #state: 当前电路的状态
         node = self.root #从根节点开始 作为一次playout
-        rewards = []
+        # rewards = []
         while True:
+            # print("current node", node)
             if node.is_leave():
+                # print("is_leave")
                 break
             #贪心算法选择最大置信上界
             action, node = node.select(self.c_puct)
+            # print("select:", action, node)
             action = circuit.dict_id2actions[action]
             circuit.insert_gate(action[0], action[1]) # action代码 转换为 门操作 insert_gate 可识别
-            product_HS = circuit.is_target()[1]
-            reward = 1 - (1 / (4 * CONFIG["n_qubit"])) * np.abs(product_HS) ** 2
-            rewards.append(reward)
+            # product_HS = circuit.is_target()[1]
+            # reward = -1 + (1 / (4 ** CONFIG["n_qubit"])) * product_HS ** 2
+            # rewards.append(reward)
 
         # state = circuit.state # current node is leave
         # print("state_MCTS ")
         #如果是叶节点 给出由神经网络提供的 所有可能的动作分布以及对当前环境的评估
         #给出动作的概率 分理出动作：概率
-        actions_probs, leaf_value = self.polic_value(circuit) # actions[0.1353, 0.3679, 0.6065, 0.2231,0.1225, 0.4493, 0.2019]] and evaluated q(s+1) of current node
-        #看电路是否满足合成要求：
-        is_target, product_HS= circuit.is_target()
-        reward = 1 - (1 / (4 * CONFIG["n_qubit"])) * np.abs(product_HS) ** 2  #当前step的reward或者cost
-        # print("is_target", is_target)
-        if not is_target:
-            node.expand(actions_probs)
-            node.Q = leaf_value
-        else:
-            node.Q = reward
-        node.count_visit += 1
+        actions_probs, leaf_value = self.policy_value(circuit) # actions[0.1353, 0.3679, 0.6065, 0.2231,0.1225, 0.4493, 0.2019]] and evaluated q(s+1) of current node
+        node.expand(actions_probs)
+        node.Q = leaf_value
+        node.count_visit += 1 #更新节点值访问次数
+        node.update_recursive(leaf_value)
+        # list_actions_probs = list(actions_probs)
+        # print("polic_value:",list_actions_probs, leaf_value )
 
-        #更新节点值访问次数
-        node.update_recursive(node.Q, rewards)
+        # #看电路是否满足合成要求：
+        # is_target, product_HS= circuit.is_target()
+        # reward = -1 + (1 / (4 ** CONFIG["n_qubit"])) * product_HS ** 2  #当前step的reward或者cost
+        # # print("is_target", is_target)
+        # if not is_target:
+        #     node.expand(actions_probs)
+        #     node.Q = leaf_value
+        # else:
+        #     node.Q = reward
+        # node.count_visit += 1
+
+
 
     def get_move_probs(self, circuit, tem=1e-3 ):
         # 获得当前状态的action的蒙特卡洛分布
@@ -132,7 +142,7 @@ class MCTS(object):
 
 class MCTSplayer(object):
 
-    def __init__(self, policy_value_fn, c_puct=5, n_playout=2000, is_selfplay=0):
+    def __init__(self, policy_value_fn, c_puct=5, n_playout=2000, is_selfplay=1):
         self.mcts = MCTS(policy_value_fn, c_puct, n_playout)
         self._is_selfplay = is_selfplay
         self.agent = "AI"
@@ -144,7 +154,7 @@ class MCTSplayer(object):
     def __str__(self):
         return 'MCTS {}'.format(self.player)
 
-    def get_action(self, circuit, num_actions, temp=1e-3, return_prob=0):
+    def get_action(self, circuit, num_actions, temp=1e-3, return_prob=1):
         # 像alphaGo_Zero论文一样使用MCTS算法返回的pi向量
         move_probs = np.zeros(num_actions) #所有actions
         # state = circuit.state
